6df6ace16c24616127ff0c0d3818336bc779832a
Pallav Agrawal <113340910+agrawalpallav@users.noreply.github.com>
2024-06-10 00:51:27 +0530

    Add files via upload

---
+++ spring-boot-001/spring-boot-001/src/main/java/com/org/springboot001/SpringBoot001Application.java
@@ -0,0 +1,14 @@
+package com.org.springboot001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot001Application.class, args);
+		System.out.println("hi");
+	}
+
+}
